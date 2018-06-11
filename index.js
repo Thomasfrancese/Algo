cb4e45d08cb73cf5760a4599ca782ee33e50e150
marion.chapuis <chapuis.marion@gmail.com>
2018-06-11 15:09:39 +0200

    Fonction initialiser le jeu

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ let tab = {
     2: [9, 10, 11, 12],
     3: [13, 14, 15, 'V']
 };
-console.log(tab);
 
 $(document).ready(function () {
-let count = 0;
+    $(".initialiser").click(function() {
+        etatInitial(tab);
+    })
+})
+
+//Fonction permettant de générer un tableau
+function etatInitial(tab) {
+    let count = 1;
     for (let j = 0; j <= 3; j++) {
-        $(".tableauBody").append("<tr id='"+ j +"'></tr>>")
+        $(".tableauBody").append("<tr id='" + j + "'></tr>>");
         for (let i = 0; i < tab[j].length; i++) {
-                $("#" + j).append('<td id="td'+count+'">'+ tab[j][i] +'</td>')
-                count++;
+            $("#" + j).append('<td id="td' + count + '">' + tab[j][i] + '</td>');
+            count++;
         }
     }
-})
+}
